a2c70be49929025e5fc32fdc79ffec250a0d832c
divya <patil.swayam102008@gmail.com>
2021-11-25 16:45:43 +0530

    third commit

---
+++ pattern/hill.py
@@ -0,0 +1,56 @@
+# for i in range(5):
+#     print (i)  
+#     for j in range(5):
+#         print (j,end = " ")
+#     print ("\r") 
+
+# rc = 4  
+ 
+# for i in range(1, rc + 1):  
+   
+#         for j in range(1, rc + 1):  
+            
+#             print("* ", end="")       
+  
+        
+#         print()  
+  
+ 
+# for i in range(4):  
+   
+#         for j in range(4):  
+            
+#             print("* ", end="")       
+         
+#         print()  
+
+# for i in range(5) :
+    
+#     for j in range(i + 1) :
+#         print("* ", end = "")
+#     print ('\r')  
+ 
+# js = 5
+# for i in range(5) :
+    
+#     for j in range(js) :
+#         print("* ", end = "")
+#     js = js - 1
+#     print ('\r')   
+
+js = 4
+for i in range(5):  
+
+        for j in range(js) :
+            print(" ", end = "")
+        js = js - 1
+            
+        for j in range(i + 1):  
+            
+            print("* ", end="")       
+         
+        print() 
+
+    
+        
+
